3dbba66eb1a4aa709bc938274839427409748590
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-07-10 19:22:59 -0400

    Create 1-Two Sum.py

---
+++ python first time/1-Two Sum.py
@@ -0,0 +1,7 @@
+hash_table = {}
+for i, num in enumerate(nums):
+    if target - num in hash_table:
+        return([hash_table[target - num], i])
+        break    
+    hash_table[num] = i
+return([])
