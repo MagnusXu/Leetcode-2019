f00c5b9399e916e2845418b839a4526256b7ef21
Magnus Xu <31779143+MagnusXu@users.noreply.github.com>
2019-07-15 15:18:45 -0400

    Create 9-palindrome number.py

---
+++ python first time/9-palindrome number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        ls = 0
+        tmp = x
+        while tmp != 0:
+            ls += 1
+            tmp = tmp // 10
+        tmp = x
+        for i in range(ls/2):
+            right = tmp % 10
+            left = tmp / (10 ** (ls - 2 * i - 1))
+            left = left % 10
+            # print left, right
+            if left != right:
+                return False
+            tmp = tmp // 10
+        return True
+    
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print s.isPalindrome(1001)
